6b4c8d38fb74ae5227a5a25b7ef2a0a9bc986d1d
1nyz <vescxve@gmail.com>
2024-06-06 16:20:06 +0300

    Artificial Neural Networks

--- MyProject.py
+++ MyProject.py
@@ -3,9 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import r2_score, confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 from tensorflow.keras import Sequential # type: ignore
 import tensorflow as tf
 
